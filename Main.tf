12322958acdbd79607f989af0bb9ba343db792d8
Oluwaseun Okeowo <97672749+OkeowoToni@users.noreply.github.com>
2023-07-14 15:40:31 +0100

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "myappservice-plan"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "mywebapp-453627 "
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  }
